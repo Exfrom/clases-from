a94756cd663e14235bf90bca10f9c692ed96f061
Exfrom <dscelis@soy.sena.edu.co>
2023-08-29 18:50:24 -0500

    se corrige codigo

--- src/my-element.js
+++ src/my-element.js
@@ -1,32 +1,105 @@
 import { LitElement, html } from "lit-element";
-export class MyElement extends LitElement{
-    
-    render(){
+
+
+export class MyElement extends LitElement {
+
+    constructor() {
+        super();
+        this.username = '';
+        this.password = '';
+        this.email = '';
+        this.show = false;
+    }
+
+
+    static get properties() {
+        return {
+            username: {
+                type: String
+            },
+            password: {
+                type: String
+            },
+            email: {
+                type: String
+            },
+            show: {
+                type: Boolean
+            }
+        }
+    }
+
+
+
+
+    setValue() {
+        // this.username = this.shadowRoot.querySelector('#username').value;
+        this.password = this.shadowRoot.querySelector('#password').value;
+        this.email = this.shadowRoot.querySelector('#email').value;
+        this.show = true;
+    }
+
+
+
+
+    render() {
         return html`
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-            <h1>Formulario de Vinculación de Candidatos</h1>
-            <form action="procesar_vinculacion.php" method="post" enctype="multipart/form-data">
-                <label for="nombre">Nombre completo:</label>
-                <input type="text" id="nombre" name="nombre" required>
-            
-                <label for="correo">Correo electrónico:</label>
-                <input type="email" id="correo" name="correo" required>
-            
-                <label for="telefono">Teléfono de contacto:</label>
-                <input type="tel" id="telefono" name="telefono" required>
-            
-                <label for="documentos">Documentos requeridos (sube aquí):</label>
-                <input type="file" id="documentos" name="documentos" accept=".pdf, .doc, .docx" required>
-    
-                <label for="comentarios">Comentarios adicionales:</label>
-                <textarea id="comentarios" name="comentarios" rows="4" cols="50"></textarea>
-            
-                <button class= "btn btn.primary">Enviar Solicitud de Vinculación</button>
-            </form>
 
-        `;
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <br>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col">
+                        <div class="card">
+                        
+                            <div class="card-body">
+                                ${this.show ? html`<div class="input-group flex-nowrap" >
+                                <span class="input-group-text">Username</span>
+                                <input type="text" class="form-control" id="username" placeholder="" >
+                            </div>` : html``}
+                                
+                                <br>
+                                <div class="input-group flex-nowrap">
+                                    <span class="input-group-text" id="">Password</span>
+                                    <input type="text" id="password" class="form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="addon-wrapping">
+                                </div>
+                                <br>
+                                <div class="input-group flex-nowrap">
+                                    <span class="input-group-text" id="">Email</span>
+                                    <input type="text" class="form-control" id="email" placeholder="Correo electronico" aria-label="Username" aria-describedby="addon-wrapping">
+                                </div>
+                                <br>
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" expand="block" @click=${() => this.setValue()}>Enviar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+
+                        
+                    
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label>${this.username}</label><br>
+                        <label>${this.password}</label><br>
+                        <label>${this.email}</label>
+                    </div>
+                </div>
+            </div>
+        ` ;
     }
 }
+
 
 customElements.define('my-element', MyElement);
